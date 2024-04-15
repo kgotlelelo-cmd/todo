2fe645e47f7972ec6803f46c12ce917acfbdb30e
Kgotlelelo Masenamela <kgotlelelo.masenamela1@vcontractor.co.za>
2024-04-15 16:00:27 +0200

    added ec2 instance cloudwatch and security groups

--- main.tf
+++ main.tf
@@ -15,15 +15,120 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.4.0"
 
   name = "Todo-VPC"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.2.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  create_database_subnet_group = true
-  database_subnet_group_name   = "database_subnet_group"
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+  database_subnets                   = ["10.0.151.0/24", "10.0.152.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "Todo_logs"
+}
+
+resource "aws_cloudwatch_log_stream" "log_stream" {
+  name           = "Todo_stream"
+  log_group_name = aws_cloudwatch_log_group.log_group.name
+}
+
+resource "aws_security_group" "todo_sg" {
+  name        = "todo-sg"
+  description = "Security group allowing HTTP and HTTPS traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_iam_role" "ec2_cloudwatch_role" {
+  name = "ec2_cloudwatch_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "cloudwatch_policy" {
+  name        = "cloudwatch_policy"
+  description = "Allows EC2 instances to send logs to CloudWatch Logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
+  policy_arn = aws_iam_policy.cloudwatch_policy.arn
+  role = aws_iam_role.ec2_cloudwatch_role.name
+}
+
+resource "aws_instance" "todo_instance" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  subnet_id       = module.vpc.public_subnets[0]
+  security_groups = [aws_security_group.todo_sg.id]
+  iam_instance_profile = aws_iam_role.ec2_cloudwatch_role.name
 }
